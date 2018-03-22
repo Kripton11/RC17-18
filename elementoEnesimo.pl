5e8a7261c5641a180b73bfde5decb44a6eba7c1f
kripton11 <juanfrancisco.pizarro@alu.uhu.es>
2018-03-22 08:45:08 +0100

    correccion elemento enesimo

--- elementoEnesimo.pl
+++ elementoEnesimo.pl
@@ -1,3 +1,3 @@
 % elemento_enesimo(+Pos, +Lista, -Elem)
 elemento_enesimo(0, [Cabeza|Resto], Cabeza).
-elemento_enesimo(Pos, [Cabeza|Resto], E):- NewPos is Pos - 1, elemento_enesimo(NewPos, Resto, E).
+elemento_enesimo(Pos, [Cabeza|Resto], E):- Pos>0, NewPos is Pos - 1, elemento_enesimo(NewPos, Resto, E).
